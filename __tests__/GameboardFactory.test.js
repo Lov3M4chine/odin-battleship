daaca2859ddd341fa5d8f8bbfc40f8cae33e82a3
William Alderman <125694577+Lov3M4chine@users.noreply.github.com>
2023-07-07 22:33:43 -0600

    Add additional tests to createGameboard

--- __tests__/GameboardFactory.test.js
+++ __tests__/GameboardFactory.test.js
@@ -1,7 +1,25 @@
 const { GameboardFactory } = require('../index.js');
 
-test('createGameboard creates a gameboard 10 x 10', () => {
-    const gameboardFactory = GameboardFactory();
+const gameboardFactory = GameboardFactory();
+
+test('createGameboard creates a gameboard', () => {
     const gameboard = gameboardFactory.createGameboard(10, 10);
     expect(gameboard.length).toBe(100);
+});
+
+test('GameboardFactory returns an array', () => {
+    const gameboard = gameboardFactory.createGameboard(10, 10);
+    expect(Array.isArray(gameboard)).toBe(true);
 })
+
+test('createGameboard throws an error when horizontal or vertical size is not an integer', () => {
+    expect(() => gameboardFactory.createGameboard("string", 10)).toThrow();
+    expect(() => gameboardFactory.createGameboard(10, "string")).toThrow();
+    expect(() => gameboardFactory.createGameboard("string", "string")).toThrow();
+})
+
+test('createGameboard throws an error when horizontal or vertical size is not > 7', () => {
+    expect(() => gameboardFactory.createGameboard(5, 10)).toThrow();
+    expect(() => gameboardFactory.createGameboard(10, 5)).toThrow();
+    expect(() => gameboardFactory.createGameboard(5, 5)).toThrow();
+});
--- index.js
+++ index.js
@@ -28,6 +28,12 @@ const ShipFactory = (length, name) => {
 
 const GameboardFactory = () => {
   function createGameboard(horizontalSize, verticalSize) {
+    if ((!Number.isInteger(horizontalSize)) || (!Number.isInteger(verticalSize))) {
+      throw new Error('Horizontal and Vertical Size must be integers')
+    }
+    if ((horizontalSize < 8) || (verticalSize < 8)) {
+      throw new Error("Horizontal and Vertical Size must be at least 7");
+    }
     let gameboard = [];
     for (let i = 0; i < (horizontalSize * verticalSize); i++) {
       gameboard.push({cell: i, isHit: false, isMiss: false, name: null})
