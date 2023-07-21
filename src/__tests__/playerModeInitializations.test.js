4a11612d1d4a1a6fb39b3b80acf0e5a4e5de0de8
William Alderman <125694577+Lov3M4chine@users.noreply.github.com>
2023-07-21 15:36:09 -0600

    Add playerModeInitializations testing

---
+++ src/__tests__/playerModeInitializations.test.js
@@ -0,0 +1,30 @@
+const { CreatePlayersForOnePlayerMode } = require("../modules/factories/CreatePlayers");
+const { initializePlaceShipsModule } = require("../modules/initializePlaceShipsModule");
+
+jest.mock("../modules/factories/CreatePlayers");
+jest.mock("../modules/initializePlaceShipsModule");
+
+const hideModeSelectContainer = jest.fn();
+const createBattlegridForPlayerOne = jest.fn();
+
+function initializeOnePlayerMode (appContext, hideModeSelectContainer, createBattlegridForPlayerOne) {
+    hideModeSelectContainer(appContext);
+    createBattlegridForPlayerOne(appContext);
+    CreatePlayersForOnePlayerMode(appContext);
+
+    initializePlaceShipsModule.initializePlaceShips(appContext);
+    console.log(`Initialization of one player mode complete.`);
+}
+
+describe('initializeOnePlayerMode', () => {
+  it('should initialize one player mode properly', () => {
+    let appContext = { /* ... */ };
+
+    initializeOnePlayerMode(appContext, hideModeSelectContainer, createBattlegridForPlayerOne);
+
+    expect(hideModeSelectContainer).toHaveBeenCalledWith(appContext);
+    expect(createBattlegridForPlayerOne).toHaveBeenCalledWith(appContext);
+    expect(CreatePlayersForOnePlayerMode).toHaveBeenCalledWith(appContext);
+    expect(initializePlaceShipsModule.initializePlaceShips).toHaveBeenCalledWith(appContext);
+  });
+});
