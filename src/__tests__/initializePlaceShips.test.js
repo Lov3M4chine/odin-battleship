7e1cc3ef5f033311323775c69f9789c2d96e8461
William Alderman <125694577+Lov3M4chine@users.noreply.github.com>
2023-07-21 17:00:57 -0600

    Create initializePlaceShipsModule tests

---
+++ src/__tests__/initializePlaceShips.test.js
@@ -0,0 +1,75 @@
+const { CreateShips } = require("../modules/factories/CreateShips");
+const { highlightShipPlacementModule } = require("../modules/highlightShipPlacementModule");
+const { initializePlaceShipsModule } = require("../modules/initializePlaceShipsModule");
+const { orientationModule } = require("../modules/orientationModule");
+const { submitButtonEventListenerModule } = require("../modules/submitButtonEventListenerModule");
+
+jest.mock("../modules/factories/CreateShips");
+jest.mock("../modules/highlightShipPlacementModule");
+jest.mock("../modules/orientationModule");
+jest.mock("../modules/submitButtonEventListenerModule");
+
+describe('initializePlaceShipsModule', () => {
+  it('should initialize place ships properly', async () => {
+    let appContext = {
+        orientation: {
+            isVertical: false
+        },
+        highlightedArray: [],
+        isPlacementValid: null,
+        cellSelected: null,
+        highlightListeners: {},
+        submitButtonListener: null,
+        playerOne: null,
+        playerTwo: null,
+        playerComputer: null,
+        currentShipName: null,
+        currentShipSize: null,
+        shipList: [],
+        // Battlegrid Size
+        horizontalSize: 10,
+        verticalSize: 10,
+
+        appElements: {
+            messageBox: { 
+                classList: { 
+                    remove: jest.fn() 
+                } 
+            },
+            horizontalButton: { 
+                classList: { 
+                    remove: jest.fn() 
+                } 
+            }
+        }
+
+    }
+    
+    const shipList = {
+      ship1: { name: 'ship1', size: 5 },
+      ship2: { name: 'ship2', size: 3 },
+    };
+
+    CreateShips.mockReturnValue(shipList);
+
+    highlightShipPlacementModule.highlightEventListenerModule = {
+      addHighlightShipEventListener: jest.fn(),
+    };
+
+    submitButtonEventListenerModule.addSubmitButtonEventListener = jest.fn().mockImplementationOnce(() => {
+      return new Promise((resolve) => {
+        resolve();
+      });
+    });
+
+    await initializePlaceShipsModule.initializePlaceShips(appContext);
+
+    expect(orientationModule.addOrientationClickEvent).toHaveBeenCalledWith(appContext);
+    expect(CreateShips).toHaveBeenCalledWith(appContext);
+    expect(highlightShipPlacementModule.highlightEventListenerModule.addHighlightShipEventListener).toHaveBeenCalledTimes(Object.keys(shipList).length);
+    expect(submitButtonEventListenerModule.addSubmitButtonEventListener).toHaveBeenCalledTimes(Object.keys(shipList).length);
+    expect(appContext.appElements.messageBox.classList.remove).toHaveBeenCalledWith("hidden");
+    expect(appContext.appElements.horizontalButton.classList.remove).toHaveBeenCalledWith("hidden");
+
+  });
+});
